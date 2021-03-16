c032dad872bdc3748602dcd035f61ec21664dc1d
LucImmerzeel <github@lucimmerzeel.nl>
2021-03-16 16:15:51 +0100

    Flask setup

---
+++ Flask-site.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+from flask import request
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def home():
+    render_template('index.html', title='Home')
+
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if valid_login(request.form['username'],
+#                        request.form['password']):
+#             return log_the_user_in(request.form['username'])
+#         else:
+#             error = 'Invalid username/password'
+#     # the code below is executed if the request method
+#     # was GET or the credentials were invalid
+#     return render_template('login.html', error=error)
+
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     user = {'username': 'Miguel'}
+#     posts = [
+#         {
+#             'author': {'username': 'John'},
+#             'body': 'Beautiful day in Portland!'
+#         },
+#         {
+#             'author': {'username': 'Susan'},
+#             'body': 'The Avengers movie was so cool!'
+#         }
+#     ]
+#     return render_template('index.html', title='Home', user=user, posts=posts)
