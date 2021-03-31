3f6f1aa9938e1d926fda6e3040775539c3a3e5a4
LucImmerzeel <github@lucimmerzeel.nl>
2021-03-31 23:22:06 +0200

    Preparing Linux 2

--- oauth-test/web_scripts/dns_records.py
+++ oauth-test/web_scripts/dns_records.py
@@ -10,14 +10,6 @@ def add_dns_record(fqdn, ip):
     if os.path.exists(os.path.join(ZONES_PATH, base_fqdn + ".zone")):
         os.remove(os.path.join(ZONES_PATH, base_fqdn + ".zone"))
 
-    sparate_records = ""
-    for separate in split_fqdn[::-1]:
-        if separate == split_fqdn[-1] or separate == split_fqdn[-2]:
-            continue
-        else:
-            f""""a": [{{"name": "{separate}", "ttl": 400, "value": "{ip}" }}]"""
-        f""""a": [{{"name": "@", "ttl": 400, "value": "{ip}" }}]"""
-
     with open(os.path.join(ZONES_PATH, base_fqdn + ".zone"), "w") as zone_file:
         zone_file.write(f""" /{{
                                         "$origin": "{base_fqdn}",
@@ -31,15 +23,9 @@ def add_dns_record(fqdn, ip):
                                             "retry": 600,
                                             "expire": 604800,
                                             "minimum": 86400}},
-
-                                        "ns": [
-                                            {{"host": "ns1.{base_fqdn}"}},
-                                            {{"host": "ns2.{base_fqdn}"}}
-                                        ],
-                                        {sparate_records}
-
-
-                                    }}""")
+                                            
+                                        "ns": [{{"host": "ns1.{base_fqdn}"}}, {{"host": "ns2.{base_fqdn}"}}],
+                                        "a": [{{"name": "@", "ttl": 400, "value": "{ip}" }}]}}""")
 
 
 def remove_dns_record():
