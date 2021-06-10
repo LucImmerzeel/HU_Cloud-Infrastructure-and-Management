cd9499ba8458286fac7d32d032edcb2c5e350b82
LucImmerzeel <github@lucimmerzeel.nl>
2021-06-10 11:01:37 +0200

    Changing DNS server 2

--- oauth-test/web_scripts/restart_dns.py
+++ oauth-test/web_scripts/restart_dns.py
@@ -191,11 +191,10 @@ EOF
 {subdomain[0]}  IN      A       {all_ip[subdomain[0] + "." + zone[0]]}"""
             except:
                 continue
-        if a_records == "":
-            a_records = f"""
-@       IN      A       {all_ip[subdomain[0] + "." + zone[0]]}"""
+
 
         a_records += f"""
+@       IN      A       {all_ip[zone[0]]}
 ns1     IN      A       127.0.0.1"""
 
         """
